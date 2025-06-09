92566011390b07e0b1a33f0bce783aa0b5b8002d
Nelya <nelya0816@gmail.com>
2025-06-09 13:47:11 +0300

    Setting up routing

--- pizza-delivery/src/app/app.routes.ts
+++ pizza-delivery/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Menu } from './pages/menu/menu';
+import { Order } from './pages/order/order';
+import { Profile } from './pages/profile/profile';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'menu', component: Menu },
+    { path: 'order', component: Order },
+    { path: 'profile', component: Profile },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+];
