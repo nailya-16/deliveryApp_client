d2bba0efc94a9d679c4cae6b3dc89c697c7ccb91
Nelya <nelya0816@gmail.com>
2025-06-13 01:32:55 +0300

    Adding standalone

--- pizza-delivery/src/app/pages/home/home.ts
+++ pizza-delivery/src/app/pages/home/home.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrls: ['./home.scss']
 })
 export class Home {
 
--- pizza-delivery/src/app/pages/order/order.ts
+++ pizza-delivery/src/app/pages/order/order.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-order',
+  standalone: true,
   imports: [],
   templateUrl: './order.html',
   styleUrl: './order.scss'
