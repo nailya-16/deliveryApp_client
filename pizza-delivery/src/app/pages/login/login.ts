fdc880df2fc7dd21bde07f22d3b0944efa56f387
Nelya <nelya0816@gmail.com>
2025-06-14 00:53:02 +0300

    - Implemented validation of `email` and `password` input fields - Added use of `Router` for navigation after successful login - Created a template with correct user interface setup for data entry - Added styles

--- pizza-delivery/src/app/pages/login/login.ts
+++ pizza-delivery/src/app/pages/login/login.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, InputTextModule, ButtonModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss']
 })
 export class Login {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
 
+  loading = false;
+  error = '';
+
+  constructor(private router: Router){}
+   async onSubmit() {
+    if (this.form.invalid) return;
+    this.loading = true;
+    this.error = '';
+    try {
+      // Здесь должен быть вызов AuthService (например, через HTTP запрос)
+      // await this.auth.login(this.form.value.email, this.form.value.password);
+      this.router.navigate(['/profile']); // после успешного входа
+    } catch (e) {
+      this.error = 'Ошибка входа';
+    }
+    this.loading = false;
+  }
 }
