a4630d0472bd83d74afca496947523eeb02d4a76
Nelya <nelya0816@gmail.com>
2025-06-18 01:07:15 +0300

    Created a Menu component to display a list of pizzas. Added a template and styles

--- pizza-delivery/src/app/pages/menu/menu.ts
+++ pizza-delivery/src/app/pages/menu/menu.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pizza } from '../../models/pizza';
+import { PizzaService } from '../../services/pizza';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.scss'
 })
-export class Menu {
+export class Menu implements OnInit {
+  pizzas: Pizza[] = [];
+  loading = true;
+  error = '';
 
+  constructor(private pizzaService: PizzaService) {}
+
+  ngOnInit(): void {
+    this.pizzaService.getPizzas().subscribe({
+      next: (data) => {
+        this.pizzas = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Ошибка загрузки меню';
+        this.loading = false;
+      }
+    });
+  }
 }
