34261eebaf44aadb61b16a9ec6a4dbafaa081e73
Nelya <nelya0816@gmail.com>
2025-06-20 00:57:19 +0300

    Added filter by ingredients

--- pizza-delivery/src/app/pages/menu/menu.ts
+++ pizza-delivery/src/app/pages/menu/menu.ts
@@ -2,18 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Pizza } from '../../models/pizza';
 import { PizzaService } from '../../services/pizza';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './menu.html',
   styleUrl: './menu.scss'
 })
 export class Menu implements OnInit {
   pizzas: Pizza[] = [];
+  filteredPizzas: Pizza[] = [];
   loading = true;
   error = '';
+  allowedIngredients: string[] = ['курица', 'грибы', 'креветки', 'ветчина', 'пепперони'];
+  selectedIngredient: string = '';
 
   constructor(private pizzaService: PizzaService) {}
 
@@ -21,6 +25,7 @@ export class Menu implements OnInit {
     this.pizzaService.getPizzas().subscribe({
       next: (data) => {
         this.pizzas = data;
+        this.applyIngredientFilter('');
         this.loading = false;
       },
       error: (err) => {
@@ -28,5 +33,16 @@ export class Menu implements OnInit {
         this.loading = false;
       }
     });
+  }
+
+  applyIngredientFilter(ingredient: string) {
+    this.selectedIngredient = ingredient;
+    if (ingredient) {
+      this.filteredPizzas = this.pizzas.filter(pizza =>
+        pizza.description.toLowerCase().includes(ingredient.toLowerCase())
+      );
+    } else {
+      this.filteredPizzas = this.pizzas;
+    }
   }
 }
