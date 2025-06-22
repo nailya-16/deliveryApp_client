fd7e96c70fa9624b99a130a579750f8074165215
Nelya <nelya0816@gmail.com>
2025-06-23 01:07:55 +0300

    Added cart typing and improved order display

--- pizza-delivery/src/app/pages/order/order.ts
+++ pizza-delivery/src/app/pages/order/order.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { PizzaService } from '../../services/pizza.service';
+import { CommonModule } from '@angular/common';
+import { Pizza } from '../../models/pizza';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order.html',
   styleUrl: './order.scss'
 })
-export class Order {
+export class Order implements OnInit{
+  pizzas: Pizza[] = [];
+  cart: { [id: string]: number } = {};
+  total: number = 0;
 
+  constructor(private cartService: CartService, private pizzaService: PizzaService){}
+
+  ngOnInit(): void {
+    this.cart = this.cartService.getCart();
+    this.pizzaService.getPizzas().subscribe(pizzas => {
+      this.pizzas = pizzas;
+      this.calculateTotal();
+    });
+  }
+
+  getCartIds(){
+    return Object.keys(this.cart);
+  }
+
+  getPizzaById(id: string | number) {
+    return this.pizzas.find(p => p.id === +id);
+  }
+
+  getPizzaName(id: string | number) {
+    return this.getPizzaById(id)?.name || '';
+  }
+
+  getPizzaPrice(id: string | number) {
+    return this.getPizzaById(id)?.price || 0;
+  }
+
+  calculateTotal() {
+    this.total = this.getCartIds().reduce((sum, id) => {
+      const pizza = this.getPizzaById(id);
+      return pizza ? sum + pizza.price * this.cart[id] : sum;
+    }, 0);
+  }
+
+  remove(pizzaId: number) {
+    this.cartService.remove(pizzaId);
+    this.cart = this.cartService.getCart();
+    this.calculateTotal();
+  }
+
+  clearCart() {
+    this.cartService.clear();
+    this.cart = {};
+    this.total = 0;
+  }
 }
