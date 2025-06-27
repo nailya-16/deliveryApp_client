129ee2a692dc0264d1439360c946ed95b0467f05
Nelya <nelya0816@gmail.com>
2025-06-27 13:22:28 +0300

    PizzaService implemented with CRUD methods via HttpClient

--- pizza-delivery/src/app/services/pizza.service.ts
+++ pizza-delivery/src/app/services/pizza.service.ts
@@ -7,10 +7,26 @@ import { Pizza } from '../models/pizza';
   providedIn: 'root'
 })
 export class PizzaService {
-  private apiUrl = 'http://localhost:3000/api/pizzas';
+  private apiUrl = 'http://localhost:3000/pizza';
   constructor(private http: HttpClient) { }
 
   getPizzas(): Observable<Pizza[]> {
     return this.http.get<Pizza[]>(this.apiUrl);
+  }
+
+  getPizzaById(id: string): Observable<Pizza> {
+    return this.http.get<Pizza>(`${this.apiUrl}/${id}`);
+  }
+
+  createPizza(pizza: Pizza): Observable<Pizza> {
+    return this.http.post<Pizza>(this.apiUrl, pizza);
+  }
+
+  updatePizza(id: string, pizza: Pizza): Observable<Pizza> {
+    return this.http.put<Pizza>(`${this.apiUrl}/${id}`, pizza);
+  }
+
+  deletePizza(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
