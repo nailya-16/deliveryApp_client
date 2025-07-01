2625a077e0a9f2f6d38469edc065f1fcfc828611
Nelya <nelya0816@gmail.com>
2025-07-01 15:26:46 +0300

    Added methods for managing the modal order window and functionality for placing an order (makeOrder) with sending data to the server. Imported FormsModule and added HttpClient for working with HTTP requests.

--- pizza-delivery/src/app/pages/order/order.ts
+++ pizza-delivery/src/app/pages/order/order.ts
@@ -3,11 +3,13 @@ import { CartService } from '../../services/cart.service';
 import { PizzaService } from '../../services/pizza.service';
 import { CommonModule } from '@angular/common';
 import { Pizza } from '../../models/pizza';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './order.html',
   styleUrl: './order.scss'
 })
@@ -15,8 +17,16 @@ export class Order implements OnInit{
   pizzas: Pizza[] = [];
   cart: { [id: string]: number } = {};
   total: number = 0;
+  userName: string ='';
+  userPhone: string = '';
+  orderSuccess: boolean = false;
+  showModal: boolean = false;
 
-  constructor(private cartService: CartService, private pizzaService: PizzaService){}
+  constructor(
+    private cartService: CartService, 
+    private pizzaService: PizzaService,
+    private http: HttpClient
+  ){}
 
   ngOnInit(): void {
     this.cart = this.cartService.getCart();
@@ -31,7 +41,7 @@ export class Order implements OnInit{
   }
 
   getPizzaById(id: string | number) {
-    return this.pizzas.find(p => p.id === +id);
+    return this.pizzas.find(p => p._id === id);
   }
 
   getPizzaName(id: string | number) {
@@ -49,7 +59,7 @@ export class Order implements OnInit{
     }, 0);
   }
 
-  remove(pizzaId: number) {
+  remove(pizzaId: string) {
     this.cartService.remove(pizzaId);
     this.cart = this.cartService.getCart();
     this.calculateTotal();
@@ -59,5 +69,36 @@ export class Order implements OnInit{
     this.cartService.clear();
     this.cart = {};
     this.total = 0;
+  }
+
+  openOrderModal() {
+    this.showModal = true;
+  }
+
+  closeOrderModal() {
+    this.showModal = false;
+  }
+
+  makeOrder() {
+    const cartArray = this.getCartIds().map(id => ({
+      pizzaName: this.getPizzaName(id),
+      quantity: this.cart[id]
+    }));
+
+    const order = {
+      customerName: this.userName,
+      customerPhone: this.userPhone,
+      cart: cartArray
+    };
+    this.http.post('http://localhost:3000/orders', order).subscribe({
+      next: () => {
+        this.orderSuccess = true;
+        this.clearCart();
+        this.closeOrderModal();
+      },
+      error: (err) => {
+        alert('Ошибка оформления заказа!');
+      }
+    });
   }
 }
