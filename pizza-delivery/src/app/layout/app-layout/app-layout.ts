a3116f78b311b83fe36a3f38acf399373bca4335
Nelya <nelya0816@gmail.com>
2025-07-17 01:25:24 +0300

    Added subscription to currentUser$ to update the menu. Implemented generation of menu items with display of user name or login button. Removed list of menu items, replaced with updated updateMenu method.

--- pizza-delivery/src/app/layout/app-layout/app-layout.ts
+++ pizza-delivery/src/app/layout/app-layout/app-layout.ts
@@ -4,6 +4,7 @@ import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-layout',
@@ -13,20 +14,51 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./app-layout.scss']
 })
 export class AppLayout implements OnInit{
-  items: MenuItem[] = [
-    { label: 'Меню', icon: 'pi pi-list', routerLink: '/menu' },
-    { label: 'Профиль', icon: 'pi pi-user', routerLink: '/profile' },
-    { label: 'Войти', icon: 'pi pi-sign-in', routerLink: '/login' },
-  ];
+  items: MenuItem[] = [];
   
   cartCount = 0;
+  user: any = null;
   
-  constructor(private cartService: CartService, private router: Router) {}
+  constructor(
+    private cartService: CartService,
+    private router: Router,
+    private auth: AuthService
+    ) {}
 
   ngOnInit() {
     this.cartService.cart$.subscribe(cart => {
       this.cartCount = Object.values(cart).reduce((sum, c) => sum + c, 0);
     });
+
+    this.auth.currentUser$.subscribe(user => {
+      this.user = user;
+      this.updateMenu();
+    });
+
+    this.updateMenu();
+  }
+
+   updateMenu() {
+    this.items = [
+      { label: 'Главная', icon: 'pi pi-home', routerLink: '/' },
+      { label: 'О нас', icon: 'pi pi-info-circle', routerLink: '/about' },
+    ];
+
+    if (this.user) {
+      // Если пользователь вошёл, показываем ник
+      this.items.push({
+        label: this.user.name,
+        icon: 'pi pi-user',
+        command: () => this.router.navigate(['/profile'])
+      });
+    } else {
+      // Если не вошёл, показываем "Войти"
+      this.items.push({
+        label: 'Войти',
+        icon: 'pi pi-sign-in',
+        command: () => this.router.navigate(['/auth'])
+      });
+    }
   }
 
   goToOrder() {
