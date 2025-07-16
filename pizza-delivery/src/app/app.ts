afcea5ff1a42d7d9606e9f6cc213d16bef6cdc5d
Nelya <nelya0816@gmail.com>
2025-07-17 01:22:22 +0300

    Added HttpClientModule and configured HTTP_INTERCEPTORS with JWTInterceptor to intercept HTTP requests.

--- pizza-delivery/src/app/app.ts
+++ pizza-delivery/src/app/app.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppLayout } from "./layout/app-layout/app-layout";
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { JWTInterceptor } from './interceptors/jwt.interceptor';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppLayout],
+  imports: [RouterOutlet, AppLayout, HttpClientModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true },
+  ]
 })
 export class App {
   protected title = 'pizza-delivery';
