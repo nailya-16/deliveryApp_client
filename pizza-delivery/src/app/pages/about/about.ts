35cbb56aae41c4057dc13d1cf8e87418ef9d790c
Nelya <nelya0816@gmail.com>
2025-07-17 01:20:27 +0300

    Adding to imports RouterModule

--- pizza-delivery/src/app/pages/about/about.ts
+++ pizza-delivery/src/app/pages/about/about.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './about.html',
   styleUrls: ['./about.scss']
 })
 export class About {
-
 }
